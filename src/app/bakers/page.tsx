d2f534c0ca781fbb4a85e5f6d24a8d0da4cf72c3
Ugo Chiori <ugochiori@gmail.com>
2023-07-27 13:00:38 +0100

    corrections

--- src/app/bakers/page.tsx
+++ src/app/bakers/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 import { useState, useEffect } from "react";
-import BakersCard from "../bakerscard/page";
-
-
+import BakersCard from "../../components/BakersCard";
 
 
 
--- src/components/BakersCard.tsx
+++ src/components/BakersCard.tsx
@@ -48,42 +48,4 @@ export default function BakersCard({
   );
 }
 
-// const BakersCard: React.FC<BakersCardProps> = ({
-//   name,
-//   business_status,
-//   rating,
-//   formatted_address,
-//   opening_hours,
-//   // photo_reference,
-//   user_ratings_total,
-// }) => {
-//   const isCurrentlyOpen = opening_hours?.open_now ?? false;
 
-//   return (
-//     <div className="flex flex-col items-center justify-center bg-gray-100 p-4 text-[black]">
-//       <h3 className="text-lg font-bold">{name}</h3>
-//       <p className="text-sm">{formatted_address}</p>
-//       <p className="text-sm">{business_status}</p>
-//       <p className="text-sm">Rating: {rating}</p>
-//       {opening_hours ? (
-//         <p className="text-sm">Open Now: {isCurrentlyOpen ? 'Yes' : 'No'}</p>
-//       ) : (
-//         <p className="text-sm">Opening hours not available</p>
-//       )}
-//       {/* <p className="text-sm">{photo_reference}</p> */}
-//       <p className="text-sm">Total Ratings: {user_ratings_total}</p>
-
-//       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//         Add to favorites
-//       </button>
-//       <button
-//         onClick={() => alert('You clicked me!')}
-//         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-//       >
-//         Details
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default BakersCard;
